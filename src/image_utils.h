27c3879bb19ad23c44871e7a6190a4571d67b6eb
TurcanS <stas220044@gmail.com>
2025-03-29 10:22:04 +0000

    Add functions for gradient generation, natural noise addition, and shape embedding in image processing

--- src/image_utils.h
+++ src/image_utils.h
@@ -2,8 +2,13 @@
 
 #include <vector>
 #include <string>
+#include <random>
 #include "../Include/lodepng.h"
 
 void encryptPassword(const std::string& password);
 std::string decryptPassword(const std::string& filename);
 std::vector<std::string> listEncFiles();
+void generateGradient(std::vector<unsigned char>& image, unsigned width, unsigned height, 
+                     const std::vector<unsigned char>& color1, const std::vector<unsigned char>& color2);
+void addNaturalNoise(std::vector<unsigned char>& image, unsigned width, unsigned height, float intensity);
+void addShapes(std::vector<unsigned char>& image, unsigned width, unsigned height, int numShapes, std::mt19937& rng);

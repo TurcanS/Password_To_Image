5a555faee15cbff1d1f0e42aefab070b89d51553
TurcanS <stas220044@gmail.com>
2026-07-27 23:13:19 +0300

    feat: add magic constants + function declarations to stego.h

--- src/stego.h
+++ src/stego.h
@@ -9,12 +9,20 @@ const size_t SALT_OFFSET = 20;
 const size_t IV_OFFSET = 100;
 const size_t HASH_OFFSET = 200;
 
+const unsigned char MAGIC_BYTES[4] = {0x50, 0x61, 0x73, 0x73};
+const size_t MAGIC_POSITIONS = 512;
+const int REDUNDANCY_FACTOR = 16;
+const size_t REDUNDANCY_STRIDE = 1024;
+
 struct EncryptedPayload {
     std::vector<unsigned char> encryptedData;
     std::string salt;
     std::vector<unsigned char> iv;
     std::string passwordHash;
 };
+
+void embedMagic(std::vector<unsigned char>& image, unsigned width, unsigned height);
+bool checkMagic(const std::vector<unsigned char>& image, unsigned width, unsigned height);
 
 void embedPayload(std::vector<unsigned char>& image, unsigned width, unsigned height,
                   const EncryptedPayload& payload, const std::string& key);
